62591b7f45e534cd745cea4e0b0a27fc29af93db
Nico60 <ndhouailly@gmail.com>
2016-09-14 19:34:01 +0800

    base: Hide arrows in Network Traffic indicators [1/2]
    Wight554: adapt for unicode indicators
    
    Change-Id: Iec9e5a111c871f2d6f0cf9f233df581066d2b460
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5661,6 +5661,12 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
+	 /**
+          * Whether to disable showing arrows in network traffic indicators
+          * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -5798,6 +5804,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOUBLE_TAP_NO_LANDSCAPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_HIDEARROW);
         }
 
         /**
