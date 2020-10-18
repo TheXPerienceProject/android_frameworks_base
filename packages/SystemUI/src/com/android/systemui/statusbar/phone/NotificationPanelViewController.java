016f0d22fb1e168dfd7f9d9847825ac6f49a29aa
maxwen <max.weninger@gmail.com>
2020-10-18 17:33:26 +0200

    SystemUI: Use DT2W setting also to wake from AOD
    Change-Id: I844e36469b0e3b0168eecb62f6e8b0e9b2bff40e
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3906,7 +3906,7 @@ public class NotificationPanelViewController extends PanelViewController {
                     return false;
                 }
 
-                if (mBarState == StatusBarState.KEYGUARD) {
+                if (mBarState == StatusBarState.KEYGUARD && !mPulsing && !mDozing) {
                     mDoubleTapGestureListener.onTouchEvent(event);
                 }
 
