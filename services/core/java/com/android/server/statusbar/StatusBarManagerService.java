60b65063d9c7ee9ccfab8844d5279ba099bc96d5
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:02:29 -0800

    core: Expose method to start assistant through Binder
    This is necessary in order to start the default assistant app from other
    system apps using platform APIs, which we need for implementing gestures
    in a modular way.
    
    Change-Id: Ic04d742acff07ce8b5b88a4296d01b1fe1036d9e
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -2259,6 +2259,17 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
         }
     }
 
+    @Override
+    public void startAssist(Bundle args) {
+        enforceStatusBarService();
+        if (mBar != null) {
+            try {
+                mBar.startAssist(args);
+            } catch (RemoteException ex) {
+            }
+        }
+    }
+
     /** @hide */
     public void passThroughShellCommand(String[] args, FileDescriptor fd) {
         enforceStatusBarOrShell();
