630aa5665eeea36307e122976172ae2f077aa061
cjh1249131356 <cjh1249131356@gmail.com>
2021-11-22 17:40:52 +0800

    SystemUI: Remove unnecessary check on status bar for DoubleTapEnabled
    * In Android 12, one click status bar will no more expand qs panel,
      So we don't need to check it. This also fixed qs panel haptic when
      config_vibrateOnIconAnimation is enabled.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelViewController.java
@@ -1359,7 +1359,7 @@ public abstract class PanelViewController {
                         onTrackingStarted();
                     }
                     if (isFullyCollapsed() && !mHeadsUpManager.hasPinnedHeadsUp()
-                            && !mCentralSurfaces.isBouncerShowing() && !mIsSbDoubleTapEnabled) {
+                            && !mCentralSurfaces.isBouncerShowing()) {
                         startOpening(event);
                     }
                     break;
