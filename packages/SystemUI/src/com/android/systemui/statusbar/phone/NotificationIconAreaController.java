b51e0dc3335f1b11076a441d7026ad1307856d36
Pranav Vashi <neobuddy89@gmail.com>
2022-01-15 17:09:09 +0530

    SystemUI: Refactor colored statusbar icons
    * Remove painful systemui checks everywhere in StatusBariconView
    * Apply NO_COLOR when colored icons are enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -68,6 +68,8 @@ public class NotificationIconAreaController implements
 
     public static final String STATUSBAR_NOTIF_COUNT =
             "system:" + Settings.System.STATUSBAR_NOTIF_COUNT;
+    public static final String STATUSBAR_COLORED_ICONS =
+            "system:" + Settings.System.STATUSBAR_COLORED_ICONS;
 
     public static final String HIGH_PRIORITY = "high_priority";
     private static final long AOD_ICONS_APPEAR_DURATION = 200;
@@ -109,6 +111,7 @@ public class NotificationIconAreaController implements
     private boolean mAodIconsVisible;
     private boolean mShowLowPriority = true;
     private boolean mShowNotificationCount = false;
+    private boolean mNewIconStyle = false;
 
     @VisibleForTesting
     final NotificationListener.NotificationSettingsListener mSettingsListener =
@@ -159,6 +162,7 @@ public class NotificationIconAreaController implements
 
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, STATUSBAR_NOTIF_COUNT);
+        tunerService.addTunable(this, STATUSBAR_COLORED_ICONS);
     }
 
     @Override
@@ -169,6 +173,14 @@ public class NotificationIconAreaController implements
                     TunerService.parseIntegerSwitch(newValue, false);
                 if (mShowNotificationCount != showIconCount) {
                     mShowNotificationCount = showIconCount;
+                    updateNotificationIcons();
+                }
+                break;
+            case STATUSBAR_COLORED_ICONS:
+                boolean newIconStyle =
+                    TunerService.parseIntegerSwitch(newValue, false);
+                if (mNewIconStyle != newIconStyle) {
+                    mNewIconStyle = newIconStyle;
                     updateNotificationIcons();
                 }
                 break;
@@ -491,7 +503,9 @@ public class NotificationIconAreaController implements
                 }
                 hostLayout.addView(v, i, params);
             }
+            v.setIconStyle(mNewIconStyle);
             v.setShowCount(mShowNotificationCount);
+            v.updateDrawable();
             v.updateIconForced();
         }
 
@@ -509,6 +523,8 @@ public class NotificationIconAreaController implements
         }
         hostLayout.setChangingViewPositions(false);
         hostLayout.setReplacingIcons(null);
+//        hostLayout.setReplacingIconsLegacy(null);
+        hostLayout.updateState();
     }
 
     /**
@@ -534,13 +550,12 @@ public class NotificationIconAreaController implements
         if (colorize) {
             color = DarkIconDispatcher.getTint(mTintAreas, v, tint);
         }
-        boolean newIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
-        if (v.getStatusBarIcon().pkg.contains("systemui") || !newIconStyle) {
+        if (v.getStatusBarIcon().pkg.contains("systemui") || !mNewIconStyle) {
             v.setStaticDrawableColor(color);
             v.setDecorColor(tint);
         } else {
-            return;
+            v.setStaticDrawableColor(StatusBarIconView.NO_COLOR);
+            v.setDecorColor(Color.WHITE);
         }
     }
 
