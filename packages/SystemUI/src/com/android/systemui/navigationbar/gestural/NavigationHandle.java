AOSPA/android_frameworks_base-old

SystemUI: Allow customizing gesture pill length [1/2]
Change-Id: I9b4b81f90f56ad6f448975b4158bed8cf2b9354c

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
@@ -26,13 +26,13 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.ContextThemeWrapper;
 import android.view.View;
-
+import android.provider.Settings;
 import com.android.settingslib.Utils;
 import com.android.systemui.R;
 import com.android.systemui.navigationbar.buttons.ButtonInterface;
 
 public class NavigationHandle extends View implements ButtonInterface {
-
+    private final Context mContext;
     protected final Paint mPaint = new Paint();
     private @ColorInt final int mLightColor;
     private @ColorInt final int mDarkColor;
@@ -47,6 +47,7 @@ public class NavigationHandle extends View implements ButtonInterface {
 
     public NavigationHandle(Context context, AttributeSet attr) {
         super(context, attr);
+        mContext = context;
         final Resources res = context.getResources();
         mRadius = res.getDimensionPixelSize(R.dimen.navigation_handle_radius);
         mBottom = res.getDimensionPixelSize(R.dimen.navigation_handle_bottom);
@@ -75,11 +76,29 @@ public class NavigationHandle extends View implements ButtonInterface {
         super.onDraw(canvas);
 
         // Draw that bar
-        int navHeight = getHeight();
         int height = mRadius * 2;
-        int width = getWidth();
-        int y = (navHeight - mBottom - height + mVerticalShift);
-        canvas.drawRoundRect(0, y, width, y + height, mRadius, mRadius, mPaint);
+        int defWidth = getWidth();
+        int lengthType = Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.GESTURE_NAVBAR_LENGTH, 0);
+        int newWidth = defWidth;
+        final Resources res = mContext.getResources();
+        switch (lengthType) {
+            case 0:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width);
+                break;
+            case 1:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width_medium);
+                break;
+            case 2:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width_long);
+                break;
+        }
+        int y = (getHeight() - mBottom - height + mVerticalShift);
+        canvas.drawRoundRect((defWidth - newWidth) / 2, y,
+            (defWidth + newWidth) / 2, y + height, mRadius, mRadius, mPaint);
     }
 
     @Override

TheXPerienceProject/android_frameworks_base

Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
@@ -26,13 +26,14 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.provider.Settings;
 
 import com.android.settingslib.Utils;
 import com.android.systemui.R;
 import com.android.systemui.navigationbar.buttons.ButtonInterface;
 
 public class NavigationHandle extends View implements ButtonInterface {
-
+    private final Context mContext;
     protected final Paint mPaint = new Paint();
     private @ColorInt final int mLightColor;
     private @ColorInt final int mDarkColor;
@@ -46,6 +47,7 @@ public class NavigationHandle extends View implements ButtonInterface {
 
     public NavigationHandle(Context context, AttributeSet attr) {
         super(context, attr);
+        mContext = context;
         final Resources res = context.getResources();
         mRadius = res.getDimensionPixelSize(R.dimen.navigation_handle_radius);
         mBottom = res.getDimensionPixelSize(R.dimen.navigation_handle_bottom);
@@ -74,11 +76,29 @@ public class NavigationHandle extends View implements ButtonInterface {
         super.onDraw(canvas);
 
         // Draw that bar
-        int navHeight = getHeight();
         int height = mRadius * 2;
-        int width = getWidth();
-        int y = (navHeight - mBottom - height);
-        canvas.drawRoundRect(0, y, width, y + height, mRadius, mRadius, mPaint);
+        int defWidth = getWidth();
+        int lengthType = Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.GESTURE_NAVBAR_LENGHT, 0);
+        int newWidth = defWidth;
+        final Resources res = mContext.getResources();
+        switch (lengthType) {
+            case 0:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width);
+                break;
+            case 1:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width_medium);
+                break;
+            case 2:
+                newWidth = res.getDimensionPixelSize(
+                    R.dimen.navigation_home_handle_width_long);
+                break;
+        }
+        int y = (getHeight() - mBottom - height + mVerticalShift);
+        canvas.drawRoundRect((defWidth - newWidth) / 2, y,
+            (defWidth + newWidth) / 2, y + height, mRadius, mRadius, mPaint);
     }
 
     @Override