d0249105ee3cdef43c14b891f8858fda97872e3e
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization [1/2]
    Author: spkal01 <kalligeross@gmail.com>
    Date:   Sat Apr 30 15:28:46 2022 +0300
    
        Inspired by dotOS
    
        neobuddy89: Clean up unused code and convert to 1-100 scale.
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Thu Jun 16 02:38:03 2022 +0530
    
        ScrimController: User Tuner API to apply custom alpha
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sun Jan 29 21:05:17 2023 +0530
    
        SystemUI: Fix multiple QS background scrim issues
    
        * Apply custom scrim alpha correctly.
        * Fixes: Flickering when using custom scrim alpha and
          expanding notifications from secured lock screen
        * Fixes: https://github.com/crdroidandroid/issue_tracker/issues/110
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [@klozz:] Add missing tuner prefs and deps to fix build issues
    
    Change-Id: Ib10c4b2e182f273bf2a22884180913763dc6acdf
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -244,6 +244,7 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.window.StatusBarWindowController;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
 import com.android.systemui.util.WallpaperController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
@@ -280,7 +281,10 @@ import dagger.Lazy;
  * </b>
  */
 @SysUISingleton
-public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
+public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, TunerService.Tunable {
+
+    private static final String QS_TRANSPARENCY =
+            "customsystem:" + Settings.System.QS_TRANSPARENCY;
 
     private static final String BANNER_ACTION_CANCEL =
             "com.android.systemui.statusbar.banner_action_cancel";
@@ -505,6 +509,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
     private final Lazy<LightRevealScrimViewModel> mLightRevealScrimViewModelLazy;
+    private final TunerService mTunerService;
 
     protected GameSpaceManager mGameSpaceManager;
 
@@ -778,6 +783,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             ActivityLaunchAnimator activityLaunchAnimator,
             InteractionJankMonitor jankMonitor,
             DeviceStateManager deviceStateManager,
+            TunerService tunerService,
             WiredChargingRippleController wiredChargingRippleController,
             IDreamManager dreamManager,
             Lazy<CameraLauncher> cameraLauncherLazy,
@@ -868,6 +874,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mUserTracker = userTracker;
         mFingerprintManager = fingerprintManager;
+        mTunerService = tunerService;
         mSysUiState = sysUiState;
 
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
@@ -933,6 +940,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mKeyguardIndicationController.init();
 
         mColorExtractor.addOnColorsChangedListener(mOnColorsChangedListener);
+
+        mTunerService.addTunable(this, QS_TRANSPARENCY);
 
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
@@ -4171,6 +4180,20 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     public boolean isKeyguardSecure() {
         return mStatusBarKeyguardViewManager.isSecure();
     }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case QS_TRANSPARENCY:
+                mScrimController.setCustomScrimAlpha(
+                        TunerService.parseInteger(newValue, 85));
+                break;
+            default:
+                break;
+         }
+    }
+
+    // End Extra BaseStatusBarMethods.
 
     @Override
     public GameSpaceManager getGameSpaceManager() {
