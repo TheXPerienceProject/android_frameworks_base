dd0f3d2718fa79ee92de72b76932b4fc15135e3f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-05 17:03:03 +0530

    display: Consider peak refresh rate in BrightnessObserver
    If the peak refresh rate set by the user is lower than the low/high
    zone refresh set in the device overlay, it switches to 60Hz. This is
    not ideal, as for example, the user may have set the peak refresh rate
    to 90Hz (while the overlay defines 120Hz as the refresh rate in the low
    brightness zone), but at low brightness the display enters 60Hz instead
    of staying at 90Hz, leading to poor UX.
    
    Use the minimum of the user-set peak brightness and the predefined
    refresh rate of the brightness zone to avoid this behaviour.
    
    Change-Id: Ia69fd5e8221bedb05b968d1eedebb4823ad4a9b7
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1597,6 +1597,7 @@ public class DisplayModeDirector {
 
         private int mRefreshRateInLowZone;
         private int mRefreshRateInHighZone;
+        private int mPeakRefreshRate;
 
         BrightnessObserver(Context context, Handler handler, Injector injector) {
             mContext = context;
@@ -1844,10 +1845,16 @@ public class DisplayModeDirector {
         @VisibleForTesting
         public void onRefreshRateSettingChangedLocked(float min, float max) {
             boolean changeable = (max - min > 1f && max > 60f);
-            if (mRefreshRateChangeable != changeable) {
+            int peakRefreshRate = Math.round(Math.max(min, max));
+            if (mRefreshRateChangeable != changeable || mPeakRefreshRate != peakRefreshRate) {
                 mRefreshRateChangeable = changeable;
-                updateSensorStatus();
-                if (!changeable) {
+                mPeakRefreshRate = peakRefreshRate;
+                if (changeable) {
+                    synchronized (mLock) {
+                        onBrightnessChangedLocked();
+                    }
+                } else {
+                    updateSensorStatus();
                     // Revoke previous vote from BrightnessObserver
                     mVotesStorage.updateGlobalVote(Vote.PRIORITY_FLICKER_REFRESH_RATE, null);
                     mVotesStorage.updateGlobalVote(Vote.PRIORITY_FLICKER_REFRESH_RATE_SWITCH, null);
@@ -2156,19 +2163,21 @@ public class DisplayModeDirector {
                 return;
             }
 
+            int refreshRateInLowZone = Math.min(mPeakRefreshRate, mRefreshRateInLowZone);
             boolean insideLowZone = hasValidLowZone() && isInsideLowZone(mBrightness, mAmbientLux);
             if (insideLowZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInLowZone, mRefreshRateInLowZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInLowZone, refreshRateInLowZone);
                 refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
             }
 
+            int refreshRateInHighZone = Math.min(mPeakRefreshRate, mRefreshRateInHighZone);
             boolean insideHighZone = hasValidHighZone()
                     && isInsideHighZone(mBrightness, mAmbientLux);
             if (insideHighZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInHighZone,
-                                mRefreshRateInHighZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInHighZone,
+                                refreshRateInHighZone);
                 refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
             }
 
